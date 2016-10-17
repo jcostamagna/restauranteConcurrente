9781e723579f941d192ee7f2da58d5aef10bf8a4
Alejandro Levinas <levinasale@gmail.com>
2016-10-17 02:03:02 -0300

    Agregada la maquina de estados para el Recepcionista #18

---
+++ src/Cocinero.cpp
@@ -0,0 +1,45 @@
+//
+// Created by ale on 17/10/16.
+//
+
+#include "Cocinero.h"
+
+
+Cocinero::~Cocinero() {
+
+}
+
+Cocinero::Cocinero() : vive(true), estado(ESPERANDO_PEDIDO) {}
+
+
+void Cocinero::run() {
+    this->rutinaCocinero();
+}
+
+void Cocinero::rutinaCocinero() {
+    while (vive) {
+        switch (estado) {
+            case ESPERANDO_PEDIDO:
+                esperandoPedido();
+            case ENTREGANDO_PEDIDO:
+                entregandoPedido();
+            case APAGON:
+                apagon();
+            default:
+                esperandoPedido();
+        }
+
+    }
+}
+
+void Cocinero::esperandoPedido() {
+
+}
+
+void Cocinero::entregandoPedido() {
+
+}
+
+void Cocinero::apagon() {
+
+}
--- src/Recepcionista.cpp
+++ src/Recepcionista.cpp
@@ -5,11 +5,45 @@
 #include <iostream>
 #include "Recepcionista.h"
 
+
+Recepcionista::Recepcionista(Pipe& clientes) : clientes(clientes),vive(true), estado(ESPERANDO) {}
+
 void Recepcionista::run() {
     this->rutinaRecepcionista();
 }
 
 void Recepcionista::rutinaRecepcionista(){
+    while (vive) {
+        switch (estado) {
+            case ESPERANDO:
+                esperando();
+            case UBICANDO_EN_LIVING:
+                ubicandoEnLiving();
+            case UBICANDO_EN_MESA:
+                ubicandoEnMesa();
+            case APAGON:
+                apagon();
+            default:
+                esperando();
+        }
+    }
+}
+
+void Recepcionista::avanzarEstado() {
+    switch (estado) {
+        //case ESPERANDO; si estaba ESPERANDO lo define la rutina si va a UBICANDO EN EL LIVING O A UBICANDO EN MESA
+        case UBICANDO_EN_LIVING:
+            estado = ESPERANDO;
+        case UBICANDO_EN_MESA:
+            estado = ESPERANDO;
+        case APAGON:
+            estado = ESPERANDO;
+        default:
+            estado = ESPERANDO;
+    }
+}
+
+void Recepcionista::esperando() {
     static const int BUFFSIZE = 100;
     // lector
     char buffer[BUFFSIZE];
@@ -24,4 +58,16 @@ void Recepcionista::rutinaRecepcionista(){
 
     this->clientes.cerrar ();
     exit ( 0 );
+}
+
+void Recepcionista::ubicandoEnLiving() {
+
+}
+
+void Recepcionista::ubicandoEnMesa() {
+
+}
+
+void Recepcionista::apagon() {
+
 }
